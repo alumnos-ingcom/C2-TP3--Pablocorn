55114093628dbbbd4595a8343eeee0d1de38e3c2
Pablocorn <cornejopablofacundo@gmail.com>
2021-06-06 18:57:32 -0300

    Update tp3ej1.py

---
+++ tp3ej1.py
@@ -0,0 +1,15 @@
+"""Escribir una funcion que solicite el ingreso de un número entero y
+vuelva a solicitarlo en caso de ingresar un valor incorrecto"""
+
+def ingreso_entero(mensaje):
+    """Recibe un número y verifica que sea entero"""
+    while True:
+        entrada = input(mensaje + " #")    
+        try:
+            entero=int(entrada)
+            print("es entero")
+            return entero
+        except ValueError as err:
+            print("error ingrese numero nuevo")
+            continue
+        return entero    
