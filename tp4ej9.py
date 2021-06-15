83561821a3b93267cf08c2e598a7e3f899fe4b4a
Pablocorn <cornejopablofacundo@gmail.com>
2021-06-14 23:24:13 -0300

    Create tp4ej9.py

---
+++ tp4ej9.py
@@ -0,0 +1,18 @@
+#Números primos
+#Escribir una función que indique con True si un numero indicado es Primo.
+
+def es_primo(numero):
+    contador=0
+    for i in range(1, numero+1):
+        if numero % i == 0:
+            contador += 1
+    
+    if contador == 2:
+        return True
+    else:
+        return False
+
+for j in range(1,101):
+        #prueba para los primeros 100 numeros
+        print("el numero",j,"es primo?",es_primo(j))
+    
