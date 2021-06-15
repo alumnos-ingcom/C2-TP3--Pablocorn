d0735be637ab820c89645e14fbbba875c65b4f4c
Pablocorn <cornejopablofacundo@gmail.com>
2021-06-14 23:24:03 -0300

    Create tp4ej4.py

---
+++ tp4ej4.py
@@ -0,0 +1,13 @@
+#Comparación de números
+#Escribir una función que reciba dos valores y retorne:
+
+#Retornar -1 si el primero es menor que el segundo
+#Retornar 0 si son iguales
+#Retornar 1 si el primero es mayor que el segundo
+def compara(numero, otro_numero):
+    if (numero<otro_numero):
+        return -1
+    elif(numero==otro_numero):
+        return 0
+    else:
+        return 1
