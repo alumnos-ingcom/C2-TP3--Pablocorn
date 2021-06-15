d63b29fb5b7b4fe1874cadc24d485d28b1988c75
Pablocorn <cornejopablofacundo@gmail.com>
2021-06-14 23:24:05 -0300

    Create tp4ej5.py

---
+++ tp4ej5.py
@@ -0,0 +1,9 @@
+def signo(numero):
+    if numero>0:
+        print("el numero es positivo")
+        
+    elif numero<0:
+        print("el numero es negativo")
+        
+    else:
+        print("el numero es cero")
