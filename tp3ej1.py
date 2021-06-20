115e6d6e578021d0547cf84e8210f704183d2f5f
Pablocorn <cornejopablofacundo@gmail.com>
2021-06-20 19:26:43 -0300

    Update tp3ej1.py

--- tp3ej1.py
+++ tp3ej1.py
@@ -1,6 +1,8 @@
 """Escribir una funcion que solicite el ingreso de un número entero y
 vuelva a solicitarlo en caso de ingresar un valor incorrecto"""
-
+class IngresoIncorrecto(Exception):
+    """Esta es la Excepcion para el ingreso incorrecto"""
+    pass 
 def ingreso_entero(mensaje):
     """Recibe un número y verifica que sea entero"""
     while True:
@@ -13,19 +15,33 @@ def ingreso_entero(mensaje):
             print("error ingrese numero nuevo")
             continue
         return entero
-#
-#def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
-#    do:
-#         esentero=ingreso_entero(mensaje)
-#        si  esentero es un entero
-#             resultado= verdadero
-#        else
-#            resultado=falso
-#    while(resultado)
 
-# def ingreso_entero_restringido(mensaje, valor_minimo=0, valor_maximo=10):
+def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
+    """Recibe un número y verifica que sea entero"""
+    while cantidad_reintentos>=0:
+        entrada = input(mensaje + " #")
+        cantidad_reintentos=cantidad_reintentos-1
+        try:
+            entero=int(entrada)
+            print("es entero")
+            return entero
+        except ValueError as err:
+            print("error ingrese numero nuevo")
+            continue
+        return entero
+def ingreso_entero_restringido(mensaje, valor_minimo=0, valor_maximo=10):
     
-    #si entero es menor a valor_maximo y menor a valor_minimo
-      #  entonces
-      #  return
-    
+    entero=ingreso_entero(mensaje);
+    if(entero>=valor_minimo and entero<=valor_maximo):
+        print("el valor es correcto")
+    else:
+        print("el valor es incorrecto")    
+
+def prueba():
+    #valor=ingreso_entero("ingrese numero entero")
+    #print(valor)
+    #valor=ingreso_entero_reintento("ingrese numero entero")   
+    #print(valor)    
+    ingreso_entero_restringido("ingrese numero entero")
+if __name__ == "__main__":
+    prueba()
